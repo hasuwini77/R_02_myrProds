1977f1e99a89d4be9a2d9c638d20ea1ab9ee25c3
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-21 16:48:41 +0100

    fixed the styling only one issue left with search

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Navbar';
 import FeaturedSong from './components/FeaturedSong';
 import { MYR_TRACKS, RANDOM_MYR } from './data/data';
 import Sidebar from './components/Sidebar';
 import CategoryFilters from './components/CategoryFilters';
-import Container from 'react-bootstrap/Container';
 
 function App() {
-
-  {/*Using state to hold the Random track selected  */ }
   const [randomTrack, setRandomTrack] = useState(RANDOM_MYR);
 
   return (
     <>
-      < Nav />
+      <Nav />
       <div className='mainContent'>
-        < Sidebar />
+        <Sidebar />
         <div className='sideContent'>
-          {randomTrack &&
-            < FeaturedSong
-              img={randomTrack.img}
-              artist={randomTrack.artist}
-              track={randomTrack.track}
-              mp3={randomTrack.mp3} />}
+          {randomTrack && <FeaturedSong
+            img={randomTrack.img}
+            artist={randomTrack.artist}
+            track={randomTrack.track}
+            mp3={randomTrack.mp3}
+          />}
         </div>
-        < CategoryFilters />
+        <CategoryFilters />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,6 @@
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { getImageURL } from '../../utils/function';
@@ -9,36 +8,28 @@ import styles from './Navbar.module.css';
 
 const Nav = () => {
     return (
-        <>
-            <Navbar className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Brand className={styles.nav} href="#home">
-                        <img
-                            alt=""
-                            src={getImageURL('headphones.png')}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        MYR
-                    </Navbar.Brand>
-                    <Row>
-                        <Col xs="auto">
-                            <Form.Control
-                                type="text"
-                                placeholder="Search"
-                                className=" mr-sm-2"
-                            />
-                        </Col>
-                        <Col xs="auto">
-                            <Button type="submit">Search</Button>
-                        </Col>
-                    </Row>
-                </Container>
-            </Navbar>
-        </>
-    )
-
+        <Navbar expand="md" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand className={styles.nav} href="#home">
+                    <img
+                        alt=""
+                        src={getImageURL('headphones.png')}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    MYR
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarNav" />
+                <Navbar.Collapse id="navbarNav" className="justify-content-end">
+                    <Form className="mb-3 mb-md-0">
+                        <Form.Control type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button type="submit" className="ml-sm-2">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
 
-export default Nav
+export default Nav;
