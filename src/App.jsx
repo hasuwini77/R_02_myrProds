8cbe0326152b92285be5d3289c3134c87d084050
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-21 09:52:24 +0100

    added the featuredSong logic to my app

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Navbar';
 import FeaturedSong from './components/FeaturedSong';
-import { MYR_TRACKS } from './data/data';
+import { MYR_TRACKS, RANDOM_MYR } from './data/data';
 
 function App() {
 
+  {/*Using state to hold the Random track selected  */ }
+  const [randomTrack, setRandomTrack] = useState(RANDOM_MYR);
 
   return (
     <>
       < Nav />
-      < FeaturedSong />
+      < FeaturedSong
+        img={randomTrack.img}
+        artist={randomTrack.artist}
+        track={randomTrack.track}
+        mp3={randomTrack.mp3} />
     </>
   )
 }
