340ed443ca181d069c8b31584609bd966ec6ab97
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 18:00:17 +0100

    added media query for home v1

--- src/components/BorderButtons/index.jsx
+++ src/components/BorderButtons/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+
 const BorderButton = ({ children, onClick, active }) => {
   const buttonStyle = {
     backgroundColor: active ? "black" : "white",
@@ -7,9 +8,14 @@ const BorderButton = ({ children, onClick, active }) => {
     borderColor: "black",
     borderRadius: "5px",
     marginBottom: "10px",
-    marginRight: "12px",
+    marginRight: "10px",
     transition: "background-color 0.3s, color 0.3s",
   };
+
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    buttonStyle.padding = "6px 10px";
+    buttonStyle.fontSize = "0.8rem";
+  }
 
   return (
     <Button variant="outline-dark" style={buttonStyle} onClick={onClick}>
