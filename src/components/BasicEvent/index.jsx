44c60d097d7c5207207bc4bb43d87f174c0263c2
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 15:35:23 +0100

    rearranged the style done

--- src/components/BasicEvent/index.jsx
+++ src/components/BasicEvent/index.jsx
@@ -7,9 +7,11 @@ function BasicEvent({ event, img, date, description }) {
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
         <Accordion.Header>
-          <span className={styles.event}>{event} </span>
-          <img src={getImageURL(img)} alt={event} style={{ width: "80px", marginRight: "10px", marginLeft: "20px", borderRadius: "5px" }} />
-          <span className={`${styles.date} float-end`}>{date}</span>
+          <div className={styles.heading}>
+            <span className={styles.event}>{event} </span>
+            <img src={getImageURL(img)} alt={event} style={{ width: "80px", marginRight: "10px", marginLeft: "20px", borderRadius: "5px" }} />
+            <span className={`${styles.date} float-end`}>{date}</span>
+          </div>
         </Accordion.Header>
         <Accordion.Body style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
           <img src={getImageURL(img)} alt={event} style={{ width: "40%", marginRight: "10px", padding: "10px", borderRadius: "25px" }} />
