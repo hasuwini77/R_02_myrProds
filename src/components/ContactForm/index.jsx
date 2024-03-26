4f3263b84ac112dabddd3a87f40421c704c980da
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 16:46:51 +0100

    added a media query for contact page mobile version

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,17 +1,20 @@
 import Form from "react-bootstrap/Form";
+import { Container } from "react-bootstrap";
 
 function ContactForm() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={6} />
-      </Form.Group>
-    </Form>
+    <Container className="d-flex justify-content-center">
+      <Form style={{ width: "60%", padding: "10px", marginTop: "17px" }}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={6} />
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
 
