12b6b0419b113433a8d0f34484e4f16a21f98ed7
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 18:04:38 +0100

    fixed the mobile view v2

--- src/components/CategoryTrack/index.jsx
+++ src/components/CategoryTrack/index.jsx
@@ -3,18 +3,20 @@ import { getAudioURL, getImageURL } from "../../utils/function";
 
 const CategoryTrack = ({ img, artist, track, mp3, onClick }) => {
   return (
-    <div className={styles.categoryTrack} onClick={onClick}>
-      <img className={styles.categoryImg} src={getImageURL(img)} />
-      <div className={styles.trackName}>
-        <h3 className={styles.catArtist}>{artist}</h3>
-        <h4 className={styles.catTrack}> {track} </h4>
+    <>
+      <div className={styles.categoryTrack} onClick={onClick}>
+        <img className={styles.categoryImg} src={getImageURL(img)} />
+        <div className={styles.trackName}>
+          <h3 className={styles.catArtist}>{artist}</h3>
+          <h4 className={styles.catTrack}> {track} </h4>
+        </div>
+        {/* Currently hiding the audio */}
+        <audio hidden controls>
+          <source src={getAudioURL(mp3)} type="audio/mpeg" />
+          Your browser does not support the audio element.
+        </audio>
       </div>
-      {/* Currently hiding the audio */}
-      <audio hidden controls>
-        <source src={getAudioURL(mp3)} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
-    </div>
+    </>
   );
 };
 
