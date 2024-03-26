29826c831830315a3c9ef59813ed55bfb761f364
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 11:29:50 +0100

    fixed the navmenu styling even more

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -15,16 +15,15 @@ const NavComponent = () => {
           <img alt="" src={getImageURL("headphones.png")} width="30" height="30" className="d-inline-block align-top" /> MYR
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse id="navbarNav">
-          <Nav className={styles.navbarNav}>
-            {" "}
-            <Nav.Item>
+        <Navbar.Collapse id="navbarNav" className="justify-content-md-end justify-content-center">
+          <Nav className="navbar-nav">
+            <Nav.Item style={{ marginLeft: "60px", fontFamily: "Shrikhand, serif" }}>
               <Nav.Link href="/">Home</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item style={{ marginLeft: "60px", fontFamily: "Shrikhand, serif" }}>
               <Nav.Link href="/event">Events</Nav.Link>
             </Nav.Item>
-            <Nav.Item>
+            <Nav.Item style={{ marginLeft: "60px", marginRight: "100px", fontFamily: "Shrikhand, serif" }}>
               <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav.Item>
           </Nav>
