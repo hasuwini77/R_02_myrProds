73fe1071eedc753a12ccf47813d317ab754e33d5
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 15:10:34 +0100

    hardcoded and styled quickly the events

--- src/components/BasicEvent/index.jsx
+++ src/components/BasicEvent/index.jsx
@@ -1,18 +1,19 @@
 import Accordion from "react-bootstrap/Accordion";
 import { getImageURL } from "../../utils/function";
+import styles from "./BasicEvent.module.css";
 
 function BasicEvent({ event, img, date, description }) {
   return (
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
         <Accordion.Header>
-          {event}
-          <img src={getImageURL(img)} alt={event} style={{ width: "50px", marginRight: "10px", marginLeft: "20px", borderRadius: "5px" }} />
-          <span className="float-end">{date}</span>
+          <span className={styles.event}>{event} </span>
+          <img src={getImageURL(img)} alt={event} style={{ width: "80px", marginRight: "10px", marginLeft: "20px", borderRadius: "5px" }} />
+          <span className={`${styles.date} float-end`}>{date}</span>
         </Accordion.Header>
         <Accordion.Body style={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
-          <img src={getImageURL(img)} alt={event} style={{ width: "50%", marginRight: "10px", padding: "10px", borderRadius: "25px" }} />
-          {description}
+          <img src={getImageURL(img)} alt={event} style={{ width: "40%", marginRight: "10px", padding: "10px", borderRadius: "25px" }} />
+          <p className={styles.description}> {description} </p>
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
--- src/components/pages/EventPage/index.jsx
+++ src/components/pages/EventPage/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import BasicEvent from "../../BasicEvent";
 import { Container } from "react-bootstrap";
+import styles from "./EventPage.module.css";
 
 const EventPage = () => {
   return (
     <>
       <Container>
         <div className="mainContent">
-          <h1>Upcoming Events</h1>
+          <h1 className={styles.title}>Upcoming Events</h1>
         </div>
         <BasicEvent event="Solstugan MYR Live " img="solstugan.jpg" date=" 2024-05-17" description="MYR will be coming back to Solstugan this summer to show some of his new music and perform a fantastic live lofi beat seshh" />
         <BasicEvent event="Humle & Humle MYR Live " img="humle.jpg" date=" 2024-06-05" description="MYR will be playing his new EP: Magic Dream at the local chill spot Humle & Humle, come to chill and drink some beers! " />
