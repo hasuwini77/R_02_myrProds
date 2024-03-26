95c5675a99eb01a9784231b5b2434ad037c30e24
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-26 14:56:57 +0100

    added some hard kind of coded basicEvents to start win

--- src/components/pages/EventPage/index.jsx
+++ src/components/pages/EventPage/index.jsx
@@ -10,6 +10,7 @@ const EventPage = () => {
           <h1>Upcoming Events</h1>
         </div>
         <BasicEvent event="Solstugan MYR Live " img="solstugan.jpg" date=" 2024-05-17" description="MYR will be coming back to Solstugan this summer to show some of his new music and perform a fantastic live lofi beat seshh" />
+        <BasicEvent event="Humle & Humle MYR Live " img="humle.jpg" date=" 2024-06-05" description="MYR will be playing his new EP: Magic Dream at the local chill spot Humle & Humle, come to chill and drink some beers! " />
       </Container>
     </>
   );
