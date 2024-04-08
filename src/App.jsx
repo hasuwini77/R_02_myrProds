90a4e1673eb9b05d44e673d7aa6bdc960a8470f8
Edwin Andermyr <andermyredwin@gmail.com>
2024-04-08 16:26:54 +0200

    cleaned up the app.jsx since the usestates was not used

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesComponent from "./components/pages/Routes";
-import { RANDOM_MYR } from "./data/data";
 import NavComponent from "./components/Navbar";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 
 function App() {
-  const [randomTrack, setRandomTrack] = useState(RANDOM_MYR);
-
   return (
     <Router>
       <div className="App">
