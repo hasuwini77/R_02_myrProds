2536b1fd45daa50df6988a96bc4b96ce07139d82
Edwin Andermyr <andermyredwin@gmail.com>
2024-04-08 16:53:39 +0200

    edited a little the events added 2 and fixed the width desktop version a little

--- src/components/pages/EventPage/index.jsx
+++ src/components/pages/EventPage/index.jsx
@@ -11,6 +11,8 @@ const EventPage = () => {
           <h1 className={styles.title}>Upcoming Events</h1>
           <BasicEvent event="Solstugan MYR Live " img="solstugan.jpg" date=" 2024-05-17" description="MYR will be coming back to Solstugan this summer to show some of his new music and perform a fantastic live lofi beat seshh" />
           <BasicEvent event="Humle & Humle MYR Live " img="humle.jpg" date=" 2024-06-05" description="MYR will be playing his new EP: Magic Dream at the local chill spot Humle & Humle, come to chill and drink some beers! " />
+          <BasicEvent event="Dinard Club MYR Live " img="talasso.jpg" date=" 2024-07-11" description="MYR will be playing his new EP: Magic Dream at the local chill spot near Dinard Beach, come to chill and drink some beers! " />
+          <BasicEvent event="Saint-Malo MYR Live " img="saintmalo.jpg" date=" 2024-08-04" description="MYR will be playing his new EP: Magic Dream at the local chill spot of Saint-Malo intra-muros! Come to chill and drink some beers! " />
         </div>
       </Container>
     </>
