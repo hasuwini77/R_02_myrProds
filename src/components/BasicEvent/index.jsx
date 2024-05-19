e1b0c95751ea2f44a9d293c38b0ccaba3ae9c870
Edwin Andermyr <hasuwini@Edwins-MacBook-Pro.local>
2024-05-19 13:35:22 +0200

    updated the BasicEvent component so it's fully responsive - working fine now

--- src/components/BasicEvent/index.jsx
+++ src/components/BasicEvent/index.jsx
@@ -10,14 +10,14 @@ function BasicEvent({ event, img, date, description }) {
         <Accordion.Item eventKey="0">
           <Accordion.Header>
             <div className={styles.heading}>
-              <span className={styles.event}>{event} </span>
+              <span className={styles.event}>{event}</span>
               <img src={getImageURL(img)} alt={event} className={styles.img} />
               <span className={`${styles.date} float-end`}>{date}</span>
             </div>
           </Accordion.Header>
           <Accordion.Body className={styles.bodyContent}>
             <img src={getImageURL(img)} alt={event} className={styles.bodyImg} />
-            <p className={styles.description}> {description} </p>
+            <p className={styles.description}>{description}</p>
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
